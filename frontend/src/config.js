78fe60375b7c9ffe230afc0ab094134771e78551
jmoura76 <jone.moura@jgmct.com>
2025-12-01 15:43:52 -0300

    Config: apontar API_BASE para backend na EC2

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_BASE = process.env.REACT_APP_API_BASE || "http://127.0.0.1:8000";
+const API_BASE = process.env.REACT_APP_API_BASE || "http://18.230.154.138:8000";
 
 export default API_BASE;
